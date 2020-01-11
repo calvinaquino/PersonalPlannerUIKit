c6d0de078dd6342c01818f1648497b6d88fe59d4
Calvin Aquino <calving.aquino@gmail.com>
2020-01-11 14:16:19 -0800

    Initial Commit

---
+++ Planejador PessoalTests/Planejador_PessoalTests.swift
@@ -0,0 +1,34 @@
+//
+//  Planejador_PessoalTests.swift
+//  Planejador PessoalTests
+//
+//  Created by Calvin Gonçalves de Aquino on 1/11/20.
+//  Copyright © 2020 Calvin Gonçalves de Aquino. All rights reserved.
+//
+
+import XCTest
+@testable import Planejador_Pessoal
+
+class Planejador_PessoalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
